ecbb6a047949902bb7efbfbe6eca9498cd968e1e
Jacob Smith <jacobsmith@Jacobs-MacBook-Pro.local>
2018-01-26 16:03:53 -0800

    Adding color picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import {OverlayModule} from '@angular/cdk/overlay';
+import {A11yModule} from '@angular/cdk/a11y';
 
 import { AppComponent } from './app.component';
+import {ColorOption, ColorPickerComponent} from './color-picker/color-picker.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorPickerComponent,
+    ColorOption
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OverlayModule,
+    A11yModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
